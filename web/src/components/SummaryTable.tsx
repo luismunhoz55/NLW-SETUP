6284894228293ccb2866a57541ff82fd92cace33
Luis <munhozluisfernando@gmail.com>
2023-09-22 18:21:41 -0300

    finish first page

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,44 @@
+import { HabitDay } from '../components/HabitDay'
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
+
+const weekDays = [
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S',
+]
+
+const summaryDays = generateDatesFromYearBeginning()
+
+export function SummaryTable() {
+    return (
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                {weekDays.map((weekDay, i) => { // retornando para cada dia da semana
+                    return (
+                        <div
+                            key={`${weekDay}-${i}`}
+                            className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center"
+                        >
+                            {weekDay}
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                {
+                    summaryDays.map((summaryDay, i) => {
+                        return (
+                            <HabitDay key={i} />
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
+}
---
+++ web/src/utils/generate-dates-from-year-beginning.ts
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs'
+
+export function generateDatesFromYearBeginning() {
+    const firstDayOfTheYear = dayjs().startOf('year')
+    const today = new Date()
+
+    let dates = []
+    let compareDate = firstDayOfTheYear
+
+    while (dates.length < 100) {
+        dates.push(compareDate.toDate())
+        compareDate = compareDate.add(1, 'day') // adicionando 1 dia a cada iteração do while
+    }
+
+    return dates
+}
