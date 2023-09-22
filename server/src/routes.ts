6785071bcddc36901da11bd67681b8acc8db56f9
luismunhoz55 <munhozluisfernando@gmail.com>
2023-09-21 22:39:32 -0300

    finish day route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -34,4 +34,50 @@ export async function appRoutes(app: FastifyInstance) {
     })
   })
 
+  app.get('/day', async (request, reply) => {
+    const getDayParams = z.object({
+      date: z.coerce.date() // transforma uma string numa data
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day') // retorna o dia da semana
+
+    // todos os hábitos possiveis do dia
+    // hábitos que já foram completados
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate()
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+
+  })
+
 }
